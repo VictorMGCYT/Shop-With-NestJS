bc7de77de97578770081cde1f1b1b0f8129da5c0
VictorMGCYT <manuelvgc2233@gmail.com>
2025-03-10 21:18:44 -0600

    FindAll with TypeORM
    10-03-2025

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, Logger } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -7,6 +7,8 @@ import { Repository } from 'typeorm';
 
 @Injectable()
 export class ProductsService {
+
+  private readonly logger = new Logger('ProductsService');
 
   constructor(
     @InjectRepository(Product)
@@ -22,14 +24,13 @@ export class ProductsService {
       return product;
       
     } catch (error) {
-      console.log(error);
-      throw new InternalServerErrorException('No puedo hacerlo papu');
+      this.handleExeptions(error);
     }
 
   }
 
-  findAll() {
-    return `This action returns all products`;
+  async findAll() {
+    return await this.productRepository.find();
   }
 
   findOne(id: number) {
@@ -42,5 +43,16 @@ export class ProductsService {
 
   remove(id: number) {
     return `This action removes a #${id} product`;
+  }
+
+
+  private handleExeptions(error: any) {
+    if(error.code === '23505'){
+      throw new BadRequestException(error.detail);
+    }
+    else{
+      this.logger.error(error);
+      throw new InternalServerErrorException('Unexpected error, chekc server logs');
+    }
   }
 }
