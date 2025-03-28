b8ded7a59a512ef92822e53ca981eceef83396c9
VictorMGCYT <manuelvgc2233@gmail.com>
2025-03-27 22:44:21 -0600

    Decorator Auth
    27-03-2025

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -10,6 +10,7 @@ import { RawHeaders } from './decorators/raw-headers.decorator';
 import { UserRoleGuard } from './guards/user-role/user-role.guard';
 import { RoleProtected } from './decorators/role-protected.decorator';
 import { ValidRoles } from './interfaces/valid-roles';
+import { Auth } from './decorators/auth.decorator';
 
 @Controller('auth')
 export class AuthController {
@@ -51,6 +52,20 @@ export class AuthController {
   @RoleProtected( ValidRoles.superUser )
   @UseGuards( AuthGuard(), UserRoleGuard )
   privateRoute2(
+    @GetUser() user: User,
+  ){
+    return {
+      ok: true,
+      user
+    }
+  }
+
+
+  @Get('private3')
+  @Auth( ValidRoles.admin )
+  @RoleProtected(  )
+  @UseGuards( AuthGuard(), UserRoleGuard )
+  privateRoute3(
     @GetUser() user: User,
   ){
     return {
